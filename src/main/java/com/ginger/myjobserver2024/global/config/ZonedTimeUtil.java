fc245331339895a929730cccb219d62e5265bfd0
jeeheaG <jeehea99@naver.com>
2024-08-08 21:39:05 +0900

    Add : create ZonedTimeUtil

--- src/main/java/com/ginger/myjobserver2024/global/config/ZonedTimeUtil.java
+++ src/main/java/com/ginger/myjobserver2024/global/config/ZonedTimeUtil.java
@@ -1,2 +1,19 @@
-package com.ginger.myjobserver2024.global.config;public class ZonedTimeUtil {
+package com.ginger.myjobserver2024.global.config;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ZonedTimeUtil {
+    private static final String zoneId ="Asia/Seoul";
+    private static final String dateTimePattern = "yyyy/MM/dd HH:mm:ss";
+
+    public static ZonedDateTime getZonedDateTime() {
+        return ZonedDateTime.now(ZoneId.of(zoneId));
+    }
+
+    public static String getZonedTimestamp() {
+        return getZonedDateTime().format(DateTimeFormatter.ofPattern(dateTimePattern));
+    }
+
 }
