393003a84090eb23874e0e0e4497bc85032c65e6
jeeheaG <jeehea99@naver.com>
2024-08-08 18:22:35 +0900

    Init : create project

---
+++ src/main/java/com/ginger/myjobserver2024/MyJobServer2024Application.java
@@ -0,0 +1,13 @@
+package com.ginger.myjobserver2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJobServer2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyJobServer2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ginger/myjobserver2024/MyJobServer2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ginger.myjobserver2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJobServer2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
