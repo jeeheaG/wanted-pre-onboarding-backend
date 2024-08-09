5be7f7ba8a1ab2523bec76fae9f9003897361795
jeeheaG <jeehea99@naver.com>
2024-08-09 16:24:45 +0900

    Add : use RequiredArgsConstructor and add getter on StatusCodeDomain

--- src/main/java/com/ginger/myjobserver2024/global/common/response/code/StatusCodeDomain.java
+++ src/main/java/com/ginger/myjobserver2024/global/common/response/code/StatusCodeDomain.java
@@ -1,19 +1,19 @@
 package com.ginger.myjobserver2024.global.common.response.code;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * 상태코드 도메인들을 일관적으로 사용하기 위해 enum 으로 관리하는 클래스
  */
+@Getter
+@RequiredArgsConstructor
 public enum StatusCodeDomain {
     COMMON("COMMON", "공통"),
     ;
 
     private final String domain;
     private final String title;
-
-    StatusCodeDomain(String domain, String title) {
-        this.domain = domain;
-        this.title = title;
-    }
 
     public String getDomainAsLabel() {
         return this.domain + "-";
