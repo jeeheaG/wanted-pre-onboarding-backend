3a16edf7e4a728f10b6e09daffc48d2abfd2ec5a
jeeheaG <jeehea99@naver.com>
2024-08-09 16:10:18 +0900

    Add : create CommonResponse

--- src/main/java/com/ginger/myjobserver2024/global/common/response/CommonResponse.java
+++ src/main/java/com/ginger/myjobserver2024/global/common/response/CommonResponse.java
@@ -1,2 +1,47 @@
-package com.ginger.myjobserver2024.global.common.response;public class CommonResponse {
+package com.ginger.myjobserver2024.global.common.response;
+
+import com.ginger.myjobserver2024.global.common.response.code.StatusCode;
+import com.ginger.myjobserver2024.global.config.ZonedTimeUtil;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+
+/**
+ * 응답 데이터를 담는 클래스
+ */
+@ToString
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class CommonResponse {
+    private final String timestamp = ZonedTimeUtil.getZonedTimestamp();
+    private final String code;
+    private final String message;
+    private final Object data;
+
+
+    public CommonResponse toResponse(StatusCode statusCode) {
+        return CommonResponse.builder()
+                .code(statusCode.getCode())
+                .message(statusCode.getMessage())
+                .build();
+    }
+
+    public CommonResponse toResponse(StatusCode statusCode, Object data) {
+        return CommonResponse.builder()
+                .code(statusCode.getCode())
+                .message(statusCode.getMessage())
+                .data(data)
+                .build();
+    }
+
+    public CommonResponse toResponse(StatusCode statusCode, String message) {
+        return CommonResponse.builder()
+                .code(statusCode.getCode())
+                .message(message)
+                .build();
+
+    }
 }
