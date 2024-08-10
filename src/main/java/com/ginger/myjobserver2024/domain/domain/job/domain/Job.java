ecf17383f6042aab01e7a8c986941696d0b6d8a7
jeeheaG <jeehea99@naver.com>
2024-08-10 18:05:40 +0900

    Add : make Job entity

---
+++ src/main/java/com/ginger/myjobserver2024/domain/domain/job/domain/Job.java
@@ -0,0 +1,51 @@
+package com.ginger.myjobserver2024.domain.domain.job.domain;
+
+import com.ginger.myjobserver2024.domain.domain.company.domain.Company;
+import com.ginger.myjobserver2024.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+/**
+ * 채용공고 엔티티
+ */
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "job")
+public class Job extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true, updatable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String title;
+
+    @Column(nullable = false, length = 30)
+    private String country;
+
+    @Column(nullable = false, length = 30)
+    private String location;
+
+    @Column(nullable = false, length = 30)
+    private String position;
+
+    private Integer reward;
+
+    @Column(length = 30)
+    private String tech;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Company company;
+
+}
