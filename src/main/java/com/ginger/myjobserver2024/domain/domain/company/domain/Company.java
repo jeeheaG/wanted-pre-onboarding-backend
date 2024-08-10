772587e39dcfc5d9dae446d11ef24d1c2475f860
jeeheaG <jeehea99@naver.com>
2024-08-10 18:05:31 +0900

    Add : make Company entity

---
+++ src/main/java/com/ginger/myjobserver2024/domain/domain/company/domain/Company.java
@@ -0,0 +1,27 @@
+package com.ginger.myjobserver2024.domain.domain.company.domain;
+
+
+import com.ginger.myjobserver2024.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * 회사 엔티티
+ */
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "company")
+public class Company extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false, length = 10)
+    private String name;
+}
