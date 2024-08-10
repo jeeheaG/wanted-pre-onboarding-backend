b60c7b5c87caa9600d2abd01f967d78ae2261ac7
jeeheaG <jeehea99@naver.com>
2024-08-10 09:56:14 +0900

    Add : make JobController

---
+++ src/main/java/com/ginger/myjobserver2024/domain/domain/job/api/JobController.java
@@ -0,0 +1,16 @@
+package com.ginger.myjobserver2024.domain.domain.job.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 채용공고관련 api 컨트롤러
+ */
+@Slf4j
+@RestController
+public class JobController {
+
+}
