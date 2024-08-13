c80130a14219959a1246bdd5f804fef8825e8269
jeeheaG <jeehea99@naver.com>
2024-08-13 21:23:26 +0900

    Add : add more status codes in CommonCode

--- src/main/java/com/ginger/myjobserver2024/global/common/response/code/CommonCode.java
+++ src/main/java/com/ginger/myjobserver2024/global/common/response/code/CommonCode.java
@@ -7,7 +7,12 @@ import org.springframework.http.HttpStatus;
  */
 public enum CommonCode implements StatusCode {
     OK(HttpStatus.OK, "2000", "정상 처리"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "5000", "서버 내부 오류")
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "5000", "서버 내부 오류"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "4000", "잘못된 요청"),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "4100", "인증되지 않음"),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "4300", "권한 부족"),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "4400", "요청 리소스를 찾을 수 없음"),
+
     ;
 
     private final HttpStatus httpStatus;
