c0faa8ccc8609d7503334a2e42fcb723e56c7d5e
jeeheaG <jeehea99@naver.com>
2024-08-15 14:51:07 +0900

    Add : make methods that create dto from entity

--- src/main/java/com/ginger/myjobserver2024/domain/job/dto/JobResponseDto.java
+++ src/main/java/com/ginger/myjobserver2024/domain/job/dto/JobResponseDto.java
@@ -2,8 +2,10 @@ package com.ginger.myjobserver2024.domain.job.dto;
 
 import com.ginger.myjobserver2024.domain.job.domain.Job;
 import lombok.*;
+import org.springframework.data.domain.Slice;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 채용공고 api 응답 dto 모음 클래스
@@ -21,6 +23,19 @@ public class JobResponseDto {
         private Integer page;
         private Integer size;
         private List<SimpleJob> jobList;
+
+        /**
+         * 채용공고 목록으로 해당 dto 클래스를 만들어주는 메서드
+         * @param jobs
+         * @return
+         */
+        public static GetJobList toDto(Slice<Job> jobs) {
+            return GetJobList.builder()
+                    .page(jobs.getNumber())
+                    .size(jobs.getSize())
+                    .jobList(SimpleJob.toDtos(jobs))
+                    .build();
+        }
     }
 
     /**
@@ -37,6 +52,34 @@ public class JobResponseDto {
         private String position;
         private Integer reward;
         private String tech;
+
+        /**
+         * 엔티티 객체로 해당 dto 클래스를 만들어주는 메서드
+         * @param job
+         * @return
+         */
+        private static SimpleJob toDto(Job job) {
+            return SimpleJob.builder()
+                    .jobId(job.getId())
+                    .companyId(job.getCompany().getId())
+                    .country(job.getCountry())
+                    .region(job.getRegion())
+                    .position(job.getPosition())
+                    .tech(job.getTech())
+                    .build();
+        }
+
+        /**
+         * dto 클래스 리스트를 만들어주는 메서드
+         * @param jobList
+         * @return
+         */
+        public static List<SimpleJob> toDtos(Slice<Job> jobList) {
+            return jobList.stream()
+                    .map(SimpleJob::toDto)
+                    .collect(Collectors.toList());
+        }
+
     }
 
     /**
