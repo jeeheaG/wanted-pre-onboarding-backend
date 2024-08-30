ef7a5e02d1b1ef20db5b2c138a14a8dd4db105c9
jeeheaG <jeehea99@naver.com>
2024-08-30 23:57:14 +0900

    Comment : edit javadoc

--- src/main/java/com/ginger/myjobserver2024/global/common/BaseTimeEntity.java
+++ src/main/java/com/ginger/myjobserver2024/global/common/BaseTimeEntity.java
@@ -8,8 +8,9 @@ import jakarta.persistence.PreUpdate;
 import java.time.ZonedDateTime;
 
 /**
- * 엔티티 클래스들의 생성시각, 수정시각 필드를 설정해주며
- * 이 클래스를 생속받은 엔티티들의 생성시각, 수정시각을 자동으로 업데이트 해주는 추상클래스
+ * 엔티티 생성, 수정시각 자동 업데이트 클래스
+ * - 엔티티 클래스들의 생성시각, 수정시각 필드를 설정해주며
+ *   이 클래스를 생속받은 엔티티들의 생성시각, 수정시각을 자동으로 업데이트 해주는 추상클래스
  */
 @MappedSuperclass //이 클래스를 상속받은 엔티티가 이 클래스의 필드들도 자신의 것으로 인식하도록 함
 public abstract class BaseTimeEntity {
